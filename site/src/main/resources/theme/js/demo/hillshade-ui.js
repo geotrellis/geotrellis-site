7f17051911b6571c72347da827356c75307f1859
Eugene Cheipesh <echeipesh@gmail.com>
2014-05-15 15:06:34 -0400

    Added sun slider

--- site/src/main/resources/theme/js/demo/hillshade-ui.js
+++ site/src/main/resources/theme/js/demo/hillshade-ui.js
@@ -3,26 +3,40 @@ define([
 ], function(model) {
 
   var time = ['07:00 AM', '08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM','01:00 PM','02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM','08:00 PM']
-  var altitude = [9.5, 21, 32.6, 44.1, 54.9, 63.6, 67.7, 64.7, 56.5, 45.9, 34.5, 22.9, 11.4, 0.6 ]
+  var altitude = [9.5, 21, 32.6, 44.1, 54.9, 63.6, 65.7, 64.7, 56.5, 45.9, 34.5, 22.9, 11.4, 0.6 ]
   var azimuth = [76.2, 85.2, 94.6, 105.7, 120.5, 143, 177, 212.4, 236.7, 252.4, 263.9, 273.5, 282.5, 291.6]
   var breaks = "0,10,20,30,40,50,60,70,80,90,100,110,120,127";
   var layer = "hills";
-  defaultState = 4;
+  var N = time.length -1;
+  var defaultState = N;
+  var maxAltitude = _.max(altitude);
 
   var updateHS = function(event, ui) {
-    var idx = ui.value
-    $("#controller-2-amount-1").val(time[idx]);
+    var idx = N - ui.value;
+    $("#sun-time").val(time[idx]);
     model.update(layer, breaks, azimuth[idx], altitude[idx], 10.0);
   };
 
   // Hillshade controllers
-  $("#controller-2-slider-1").slider({
-    min: 0, max: time.length - 1, value: defaultState,
-    stop: updateHS
+  $("#sun-time").val(time[N-defaultState]);
+
+  $('.sun-slider-content').slider({
+    'animate': false,
+    'min': 0, 'max': N,
+    'value' : defaultState,
+    'orientation' : 'horizontal',
+    'stop': updateHS,
+    'slide': function(e, ui){
+      var i = N - ui.value;      
+      var percentLeft = altitude[i] / maxAltitude * 100;
+        
+      $(this).children('.ui-slider-handle').css('background-position', '0 ' + (100 - percentLeft) +'%'); //set css sprite      
+      console.log(altitude[i],percentLeft);
+    }
   });
-  $("#controller-2-amount-1").val(time[defaultState]);
+
 
   return {
-    'init': function() { model.update(layer, breaks, azimuth[defaultState], altitude[defaultState], 10.0);}
+    'init': function() { model.update(layer, breaks, azimuth[N-defaultState], altitude[N-defaultState], 10.0);}
   }
 });
