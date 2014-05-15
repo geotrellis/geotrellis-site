1fe2dd54b39754c66599c3b7656382bfbf33204a
Jeff Frankl <jfrankl@azavea.com>
2014-05-15 17:12:46 -0400

    Use Azavea's mapbox account for real this time

--- site/src/main/resources/theme/js/demo/maps.js
+++ site/src/main/resources/theme/js/demo/maps.js
@@ -1,8 +1,8 @@
 define([], function(){
   var maps = [
-    L.mapbox.map('map-1', 'azavea.i8f6m5el'),
-    L.mapbox.map('map-2', 'azavea.i8f6m5el'),
-    L.mapbox.map('map-3', 'azavea.i8f6m5el')
+    L.mapbox.map('map-1', 'azavea.i8f80o61'),
+    L.mapbox.map('map-2', 'azavea.i8f80o61'),
+    L.mapbox.map('map-3', 'azavea.i8f80o61')
   ];
 
   maps[0].setView([39.99, -75.21], 12);
