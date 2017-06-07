a8baddec62d110fe4020944e630894c2f8acf712
Taylor Nation <tnation14@gmail.com>
2017-06-07 11:31:44 -0400

    Add Terraform resources for Website ECS service
    - Use terraform-aws-ecs-web-service module to create a service that runs gtsite-nginx and gtsite-service.
    - Cache tiles using CloudFront.
    - Manage remote state with an S3 backend.
    - Load the core infrastructure remote state so that we can read outputs:
        - VPC ID
        - Container Instance security group & IAM roles
        - Route53 Hosted Zone ID.
        - Cloudfront Logs bucket name
    - DNS records for Website and CDN
    - Firewall rules for Web Services

--- deployment/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "service_elb_ip" {
-  value = "${aws_elb.gtsite.dns_name}"
-}
--- deployment/terraform/state-persistence.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "gtsite" {
-    backend = "s3"
-    config {
-        bucket = "aws-state"
-        key = "geotrellis-site/GT-SITE.tfstate"
-        region = "us-east-1"
-    }
-}
