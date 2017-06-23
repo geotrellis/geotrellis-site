e62aa3f122a26644afc4b3f39482dc4e98055b07
Taylor Nation <tnation14@gmail.com>
2017-06-23 12:50:27 -0400

    Enable transit demo in carousel

--- static/js/demo/main.js
+++ static/js/demo/main.js
@@ -31,6 +31,8 @@ function(wo_ui, hs_ui, transit, maps){
           switch (to) {
             case 1: hs_ui.init();
               break;
+            case 2: transit.init();
+              break;
           }
         }
         
