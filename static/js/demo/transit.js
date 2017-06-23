6d73849ff4910f5b77807452af74c0df059712ec
Taylor Nation <tnation14@gmail.com>
2017-06-23 09:07:10 -0400

    Update transit demo endpoint

--- static/js/demo/transit.js
+++ static/js/demo/transit.js
@@ -33,7 +33,7 @@ define([
         mapLayer = null;
     }
 
-    mapLayer = new L.TileLayer.WMS("http://transit.geotrellis.com/api/travelshed/wms", {
+    mapLayer = new L.TileLayer.WMS("https://transit.geotrellis.io/api/travelshed/wms2", {
                     latitude: marker.getLatLng().lat,
                     longitude: marker.getLatLng().lng,
                     time: time,
